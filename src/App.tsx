4e9cdc9df04cb7f0bf126d1f38267f674a39ebf0
Kavindu Gayan Samarasinghe <115790355+Kavindu95@users.noreply.github.com>
2026-06-19 16:26:30 +0530

    Update App.tsx part 2

--- src/App.tsx
+++ src/App.tsx
@@ -143,7 +143,7 @@ const AppContent: React.FC = () => {
           <section className="relative bg-gray-950 text-white py-20 sm:py-28 overflow-hidden">
             {/* Background Image of beautiful Sri Lanka roads */}
             <div className="absolute inset-0 z-0 opacity-25">
-              //coast SL
+            
               <div className="absolute inset-0 bg-gradient-to-t from-gray-950 via-gray-950/80 to-transparent" />
             </div>
 
